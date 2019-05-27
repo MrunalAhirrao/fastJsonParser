6d1a9e14edb77396a3759962cbbb01f603a7d478
Mrunal Ahirrao <42505441+MrunalAhirrao@users.noreply.github.com>
2019-05-27 06:52:00 +0530

    Added example with very small JSON

--- example.c
+++ example.c
@@ -3,16 +3,18 @@
 #include "fastJsonParser.h"
 
 char *JSON_STRING_data =
-		"{\"user\":\"johndoe\",\"admin\":false,\"uid\":1000,\"groups\":[\"users\",\"wheel\",\"audio\",\"video\"]}/";
+		"{\"user\":\"johndoe\",\"admin\":false,\"uid\":1000,\"groups\":[\"users\",\"wheel\",\"audio\",\"video\"]}";
 
 char *newjson =
-		"{\"user\":{\"ListOfIDUs\":[1,2,3,4,5],\"IDU_Info\":[{\"IDU_Num\":1,\"Fan speed\":\"High\",\"TSC\":1},{\"IDU_Num\":2,\"Fan speed\":\"High\",\"TSC\":1},{\"IDU_Num\":3,\"Fan speed\":\"High\",\"TSC\":1},{\"IDU_Num\":4,\"Fan speed\":\"High\",\"TSC\":1}]}}/";
+		"{\"user\":{\"ListOfIDUs\":[1,2,3,4,5],\"IDU_Info\":[{\"IDU_Num\":1,\"Fan speed\":\"High\",\"TSC\":1},{\"IDU_Num\":2,\"Fan speed\":\"High\",\"TSC\":1},{\"IDU_Num\":3,\"Fan speed\":\"High\",\"TSC\":1},{\"IDU_Num\":4,\"Fan speed\":\"High\",\"TSC\":1}]}}";
+
+char *smalljson = "{\"data\":{\"List\":[1,2,3,4,5]}}";
 
 fast_json_parser parser;
 
 int main(void) {
-	StartAddr = (uint8_t*) newjson;
-	EndAddr = StartAddr + 250;
+	StartAddr = (uint8_t*) smalljson;
+	EndAddr = StartAddr + 28;
 	json_parser_init();
 	while(1) {
 		parser = json_parse();
