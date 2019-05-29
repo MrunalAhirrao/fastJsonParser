c7cf957ec1154a1aea9cf29fac19e1aacbbba946
Mrunal Ahirao <42505441+MrunalAhirrao@users.noreply.github.com>
2019-05-29 07:51:11 +0530

    Update example.c
    Added printing of different parsed data

--- example.c
+++ example.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 
 #include "fastJsonParser.h"
 
@@ -12,12 +13,39 @@ char *smalljson = "{\"data\":{\"List\":[1,2,3,4,5]}}";
 
 fast_json_parser parser;
 
+uint8_t someBuff[1000],cntr = 0;
+
 int main(void) {
-	StartAddr = (uint8_t*) smalljson;
-	EndAddr = StartAddr + 28;
+	StartAddr = (uint8_t*) newjson;
+	EndAddr = StartAddr + 212;
 	json_parser_init();
 	while(1) {
 		parser = json_parse();
+
+		while(parser.Start<parser.End + 1){
+			someBuff[cntr] = *parser.Start;
+			parser.Start++;
+			cntr++;
+		};
+
+		if(parser.parsed_type == JSON_OBJ){
+			printf("JSON OBJECT is:%s\n",someBuff);
+		}else if(parser.parsed_type == JSON_STRING){
+			printf("JSON STRING is:%s\n",someBuff);
+		}else if(parser.parsed_type == JSON_ARRAY){
+			printf("JSON ARRAY is:%s\n",someBuff);
+		}else if(parser.parsed_type == JSON_BOOL){
+			printf("JSON BOOLEAN is:%s\n",someBuff);
+		}else if(parser.parsed_type == JSON_PRIMITIVE){
+			printf("JSON PRIMITIVE is:%s\n",someBuff);
+		}else if(parser.parsed_type == JSON_END){
+			printf("JSON Ended here %s\n",someBuff);
+		}
+
+
+		cntr = 0;
+		memset(someBuff,0,1000);
+
 		if(parser.parsed_type == JSON_END)break;
 	}
 	return 0;
